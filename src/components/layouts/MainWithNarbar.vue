13b955613eaeaddb09b0e74df7827051d7e84d45
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-03-30 11:35:43 +0700

    feat: add MainWithNarbar and Narbar components for layout structure and navigation

---
+++ src/components/layouts/MainWithNarbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import cn from "../../utils/cn";
+import Narbar from "./Narbar.vue";
+
+const props = defineProps<{ class?: string }>();
+</script>
+
+<template>
+  <Narbar />
+  <main :class="cn('mt-13 md:mt-20', props.class)">
+    <slot />
+  </main>
+</template>
