87aa8c7dd2041f8448b135ffa8837190de374cb2
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-03-31 12:33:57 +0700

    feat: add InputForm and Input components with validation support using vee-validate

---
+++ src/components/form/InputForm.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { ErrorMessage, useFieldError } from "vee-validate";
+import Input from "../ui/Input.vue";
+
+interface Props {
+  name: string;
+  label: string;
+  value?: string;
+  type?: "text" | "email" | "password";
+  placeholder?: string;
+  autocomplete?: string;
+  disabled?: boolean;
+  rules?: string;
+  required?: boolean;
+  class?: string;
+}
+
+const props = defineProps<Props>();
+const errorMessage = useFieldError(props.name);
+
+const isFieldError = computed(() => Boolean(errorMessage.value));
+
+const inputRules = computed(() => {
+  const rules = props.rules ?? "";
+
+  if (props.required) {
+    return "required|" + rules;
+  }
+
+  return rules;
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label
+      :for="props.name + '-id'"
+      class="flex gap-0.5 w-fit style-headline-5"
+    >
+      {{ props.label }}
+      <span v-if="props.required" class="text-red">*</span>
+    </label>
+    <Input
+      :id="props.name"
+      :name="props.name"
+      :type="props.type"
+      :value="props.value"
+      :placeholder="props.placeholder"
+      :autocomplete="props.autocomplete"
+      :rules="inputRules"
+      :disabled="props.disabled"
+      :isError="isFieldError"
+      :class="props.class"
+    />
+    <ErrorMessage :name="props.name" class="style-body-4 text-red" />
+  </div>
+</template>
---
+++ src/components/ui/Input.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Field } from "vee-validate";
+import { ExclamationCircleIcon } from "../icons";
+import cn from "../../utils/cn";
+
+interface Props {
+  name: string;
+  value?: string;
+  type?: "text" | "email" | "password";
+  placeholder?: string;
+  autocomplete?: string;
+  disabled?: boolean;
+  isError?: boolean;
+  rules?: string;
+  class?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div :class="cn('relative', props.class)">
+    <Field
+      :id="props.name + '-id'"
+      :name="props.name"
+      :type="props.type"
+      :value="props.value"
+      :placeholder="props.placeholder"
+      :autocomplete="props.autocomplete"
+      :rules="props.rules"
+      :disabled="props.disabled"
+      :class="
+        cn(
+          'w-full px-4 py-2.5 style-body-2  text-gray-950 bg-white rounded-lg ring ring-inset ring-gray-300 transition-all outline-none',
+          'focus-visible:ring-blue-600',
+          'placeholder:text-gray-700',
+          'disabled:text-gray-400 disabled:bg-gray-100 disabled:ring-gray-300',
+          isError && 'ring-red focus-visible:ring-red',
+        )
+      "
+    />
+    <component
+      v-if="isError"
+      :is="ExclamationCircleIcon"
+      class="absolute top-3.5 right-3.5 size-4 text-red"
+    />
+  </div>
+</template>
